6cd2ee0afcb3c6954993853a25bcdc50eed93036
jby9725 <jby9725@gmail.com>
2024-11-05 14:21:03 +0900

    project setting & URL 단축 방식 설정

---
+++ src/main/java/com/koreait/surl_project_11/SurlController.java
@@ -0,0 +1,50 @@
+package com.koreait.surl_project_11;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class SurlController {
+
+    private List<Surl> surls = new ArrayList<>();
+    private long surlsLastId;
+
+    @GetMapping("/add")
+    @ResponseBody
+    public Surl add(String body, String url) {
+        Surl surl = Surl.builder()
+                .id(++surlsLastId)
+                .body(body)
+                .url(url)
+                .build();
+
+        surls.add(surl);
+
+        return surl;
+    }
+
+    @GetMapping("/s/{body}/**")
+    @ResponseBody
+    public String add(
+            @PathVariable String body,
+            HttpServletRequest req
+    ) {
+        String requestURI =  req.getRequestURI();
+
+        if(req.getQueryString() != null) {
+            requestURI += "?" + req.getQueryString();
+        }
+
+        String[] urlBits = requestURI.split("/", 4);
+
+        requestURI = urlBits[3];
+
+        return requestURI;
+    }
+}
