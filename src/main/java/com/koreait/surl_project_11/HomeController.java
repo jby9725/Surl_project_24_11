4f36f0cac101a2f8373b66bc09fd6bb40c9c61e2
jby9725 <jby9725@gmail.com>
2024-11-07 10:35:14 +0900

    개발환경과 운영환경의 설정파일 분리

--- src/main/java/com/koreait/surl_project_11/HomeController.java
+++ src/main/java/com/koreait/surl_project_11/HomeController.java
@@ -1,5 +1,6 @@
 package com.koreait.surl_project_11;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
+    @Value("${custom.site.name}")
+    private String customSiteName;
+
     @GetMapping("/")
     @ResponseBody
     public String showMain() {
-        return "Main";
+        return "Hello. Main on " + customSiteName;
     }
 }
