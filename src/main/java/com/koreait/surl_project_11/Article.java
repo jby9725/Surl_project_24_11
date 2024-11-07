ceea15f2d7d63bb9ed4e00b733234cd19f3eed13
jby9725 <jby9725@gmail.com>
2024-11-07 15:51:35 +0900

    Article 클래스로 article 테이블 생성, JPA가 실행하는 SQL 로깅

---
+++ src/main/java/com/koreait/surl_project_11/Article.java
@@ -0,0 +1,21 @@
+package com.koreait.surl_project_11;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity // 이거 클래스 가지고 테이블 만들 거임!
+public class Article {
+
+    @Id // 이 필드를 PK로 쓸거임!
+    @GeneratedValue(strategy = IDENTITY) // 이거 AUTO_INCREMENT 로 만들 거임!
+    private long id;
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String body; // 이 필드의 자료형을 TEXT로 지정 할거야!
+    private String body2;
+    private String body3;
+}
