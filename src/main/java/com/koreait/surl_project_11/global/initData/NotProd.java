30a77ab97f2a70769836ae7aa776a80e82f49573
jby9725 <jby9725@gmail.com>
2024-11-08 13:27:26 +0900

    Service 도입

--- src/main/java/com/koreait/surl_project_11/global/initData/NotProd.java
+++ src/main/java/com/koreait/surl_project_11/global/initData/NotProd.java
@@ -1,7 +1,7 @@
 package com.koreait.surl_project_11.global.initData;
 
 import com.koreait.surl_project_11.domain.article.article.entity.Article;
-import com.koreait.surl_project_11.domain.article.article.repository.ArticleRepository;
+import com.koreait.surl_project_11.domain.article.article.service.ArticleService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +20,6 @@ import java.util.Optional;
 @Configuration // 빈이 자동화되서 실행된다 >> 이 어노테이션이 붙어있어야 실행한다.
 @RequiredArgsConstructor
 public class NotProd {
-
-    private final ArticleRepository articleRepository;
-
     // 트랜잭션을 제대로 사용 하기 위해 아래와 같은 작업이 필요 하다. 클래스 객체를 가지고 있어야 하는 것과 this의 차이를 모르면 그냥 외우자..
     // 상세 설명 :
     // this를 통한 객체 내부에서의 메서드 호출은 @Transactional를 작동시키지 않는다.
@@ -33,14 +30,19 @@ public class NotProd {
     @Autowired
     private NotProd self;
 
+    private final ArticleService articleService;
+
+//    @Autowired
+//    private ArticleRepository articleRepository;
+
     @Bean // 빈을 등록한다. 스트링부트에. 개발자가 new 하지 않아도 스프링부트가 직접 관리하는 객체. 실행될때 자동으로 올라간다.
     public ApplicationRunner initNotProd() {
         return args -> {
             self.work1();
             self.work2(); //this. 가 생략 되어 있다.
-            self.work3();
-            self.work4();
-            self.work5();
+//            self.work3();
+//            self.work4();
+//            self.work5();
         };
     }
 
@@ -54,7 +56,7 @@ public class NotProd {
 
         // DB 쪽에서 직접 TRUNCATE : AUTO_INCREMENT 값이 초기화 된다.
 
-        long countAll = articleRepository.count();
+        long countAll = articleService.count();
 
         System.out.println("countAll: " + countAll);
 
@@ -77,8 +79,11 @@ public class NotProd {
         System.out.println(articleFirst.getId());
         System.out.println(articleSecond.getId());
 
-        articleRepository.save(articleFirst);
-        articleRepository.save(articleSecond);
+//        articleRepository.save(articleFirst);
+//        articleRepository.save(articleSecond);
+
+        articleService.write("제목1", "내용1");
+        articleService.write("제목2", "내용2");
 
         System.out.println("articleFirst.getId() : " + articleFirst.getId());
         System.out.println("articleSecond.getId() : " + articleSecond.getId());
@@ -87,7 +92,7 @@ public class NotProd {
         articleFirst.setTitle("수정된 제목");
 
         // 하나 삭제하려고 할 때
-        articleRepository.delete(articleSecond);
+        articleService.delete(articleSecond);
     }
 
 
@@ -97,9 +102,9 @@ public class NotProd {
         // 차이점
         //	- List : 0 ~ N 개 가능
         //	- Optional : 0 ~ 1 개 가능
-        Optional<Article> articleOptional = articleRepository.findById(1L);
+        Optional<Article> articleOptional = articleService.findById(1L);
 
-        List<Article> articleList = articleRepository.findAll();
+        List<Article> articleList = articleService.findAll();
 
     }
 
@@ -114,7 +119,7 @@ public class NotProd {
         // where id IN (1,2,3)
         // order by title DESC, id asc;
         // articleRepository.findByIdInOrderByTitleDescIdAsc(ids); 또는
-        articleRepository.findByIdInOrderByTitleDescIdAsc(List.of(1L, 2L, 3L));
+//        articleRepository.findByIdInOrderByTitleDescIdAsc(List.of(1L, 2L, 3L));
     }
 
     @Transactional
@@ -123,7 +128,7 @@ public class NotProd {
         // select *
         // from article
         // where title LIKE '%안녕%';
-        articleRepository.findByTitleContaining(keyword);
+//        articleRepository.findByTitleContaining(keyword);
     }
 
     @Transactional
@@ -134,6 +139,6 @@ public class NotProd {
         // from article
         // where title = '안녕'
         // AND body = '잘가';
-        articleRepository.findByTitleAndBody(title, body);
+//        articleRepository.findByTitleAndBody(title, body);
     }
 }
