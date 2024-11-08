7f416182eb3b123e7f73cccbb89e4d6b42ef3cdc
jby9725 <jby9725@gmail.com>
2024-11-08 15:47:29 +0900

    MappedSuperClass 도입

--- src/main/java/com/koreait/surl_project_11/domain/article/article/entity/Article.java
+++ src/main/java/com/koreait/surl_project_11/domain/article/article/entity/Article.java
@@ -1,14 +1,13 @@
 package com.koreait.surl_project_11.domain.article.article.entity;
 
-import jakarta.persistence.*;
+import com.koreait.surl_project_11.global.jpa.entity.BaseTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity // 이거 클래스 가지고 테이블 만들 거임!
 // Entity를 사용했으면, NoArgsConstructor는 있어야 함.
@@ -16,18 +15,10 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 // 하지만 NoArgsConstructor 추가하면 빌더가 뭐라고 하고, 그걸 피하기 위해서는 AllArgsConstructor 가 있어야 함.
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
 @EntityListeners(AuditingEntityListener.class) // @CreatedDate 와 @LastModifiedDate 쓸려면 붙여야 함.
-public class Article {
-
-    @Id // 이 필드를 PK로 쓸거임!
-    @GeneratedValue(strategy = IDENTITY) // 이거 AUTO_INCREMENT 로 만들 거임!
-    private long id;
-    @CreatedDate
-    private LocalDateTime createDate;
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
+public class Article extends BaseTime {
     private String title;
     @Column(columnDefinition = "TEXT")
     private String body; // 이 필드의 자료형을 TEXT로 지정 할거야!
--- src/main/java/com/koreait/surl_project_11/domain/member/member/entity/Member.java
+++ src/main/java/com/koreait/surl_project_11/domain/member/member/entity/Member.java
@@ -1,14 +1,13 @@
 package com.koreait.surl_project_11.domain.member.member.entity;
 
-import jakarta.persistence.*;
+import com.koreait.surl_project_11.global.jpa.entity.BaseTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity // 이거 클래스 가지고 테이블 만들 거임!
 // Entity를 사용했으면, NoArgsConstructor는 있어야 함.
@@ -16,19 +15,11 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 // 하지만 NoArgsConstructor 추가하면 빌더가 뭐라고 하고, 그걸 피하기 위해서는 AllArgsConstructor 가 있어야 함.
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@AllArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
 @EntityListeners(AuditingEntityListener.class) // @CreatedDate 와 @LastModifiedDate 쓸려면 붙여야 함.
 @ToString
-public class Member {
-
-    @Id // 이 필드를 PK로 쓸거임!
-    @GeneratedValue(strategy = IDENTITY) // 이거 AUTO_INCREMENT 로 만들 거임!
-    private long id;
-    @CreatedDate
-    private LocalDateTime createDate;
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
+public class Member extends BaseTime {
     @Column(unique = true)
     private String username;
 //    @Column(columnDefinition = "TEXT") // 이 필드의 자료형을 TEXT로 지정 할거야!
