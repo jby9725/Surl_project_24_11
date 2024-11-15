26f8ec673f906ce2b90cba3fa7a1adcda283c634
jby9725 <jby9725@gmail.com>
2024-11-15 13:37:13 +0900

    Ut class 생성-String 유효성 검사 추가

--- src/main/java/com/koreait/surl_project_11/domain/member/member/controller/MemberController.java
+++ src/main/java/com/koreait/surl_project_11/domain/member/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.koreait.surl_project_11.domain.member.member.entity.Member;
 import com.koreait.surl_project_11.domain.member.member.service.MemberService;
 import com.koreait.surl_project_11.global.exceptions.GlobalException;
 import com.koreait.surl_project_11.global.rsData.RsData;
+import com.koreait.surl_project_11.standard.util.Ut;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,17 +19,21 @@ public class MemberController {
     @ResponseBody
     public RsData join(String username, String password, String nickname) {
 
-        if(username == null || username.isBlank()) {
+
+
+        if(Ut.str.isBlank(username)) {
+            // 예외 상황이 발생 했을 때, return 보다는 throw 가 낫다.
             // return RsData.of("400-1", "username 입력하세요.");
             throw new GlobalException("400-1", "username 입력하세요.");
+
         }
 
-        if(password == null || password.isBlank()) {
+        if(Ut.str.isBlank(password)) {
             // return RsData.of("400-1", "password 입력하세요.");
             throw new GlobalException("400-2", "password 입력하세요.");
         }
 
-        if(nickname == null || nickname.isBlank()) {
+        if(Ut.str.isBlank(nickname)) {
             // return RsData.of("400-1", "nickname 입력하세요.");
             throw new GlobalException("400-3", "nickname 입력하세요.");
         }
---
+++ src/main/java/com/koreait/surl_project_11/standard/util/Ut.java
@@ -0,0 +1,17 @@
+package com.koreait.surl_project_11.standard.util;
+
+public class Ut {
+    // 접근할 때 Ut.str.isBlank()
+    public static class str {
+        public static boolean isBlank(String str) {
+            return str == null || str.isBlank();
+        }
+        // 반대 기능 (습관적으로 작성)
+        // (비슷한 이름으로 같은 기능) public static boolean hasLength(String str) {}
+        public static boolean isNotBlank(String str) {
+            return !isBlank(str);
+        }
+    }
+
+    public static class json {}
+}
