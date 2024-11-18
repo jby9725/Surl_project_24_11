f887c9d1d594f20c55185d7694eaee613c1cf6f8
jby9725 <jby9725@gmail.com>
2024-11-18 13:39:12 +0900

    ApiV1MemberController 생성

--- src/main/java/com/koreait/surl_project_11/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/koreait/surl_project_11/domain/member/member/controller/ApiV1MemberController.java
@@ -5,45 +5,57 @@ import com.koreait.surl_project_11.domain.member.member.service.MemberService;
 import com.koreait.surl_project_11.global.exceptions.GlobalException;
 import com.koreait.surl_project_11.global.rsData.RsData;
 import com.koreait.surl_project_11.standard.util.Ut;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
 @Slf4j
-public class MemberController {
+public class ApiV1MemberController {
     private final MemberService memberService;
 
-    @GetMapping("/join")
-    @ResponseBody
-    public RsData<Member> join(String username, String password, String nickname) {
+    @AllArgsConstructor
+    @Getter
+    public static class MemberJoinReqBody {
+        private String username;
+        private String password;
+        private String nickname;
+    }
 
-        if (Ut.str.isBlank(username)) {
+    // CRUD
+
+    // POST /api/v1/members/join
+    @PostMapping("/join")
+    public RsData<Member> join(
+            @RequestBody MemberJoinReqBody requestBody
+    ) {
+        // 같은 클래스 안에 있으니 바로 접근 가능. username.
+        if (Ut.str.isBlank(requestBody.username)) {
             // 예외 상황이 발생 했을 때, return 보다는 throw 가 낫다.
             // return RsData.of("400-1", "username 입력하세요.");
             throw new GlobalException("400-1", "username 입력하세요.");
         }
 
-        if (Ut.str.isBlank(password)) {
+        if (Ut.str.isBlank(requestBody.password)) {
             // return RsData.of("400-1", "password 입력하세요.");
             throw new GlobalException("400-2", "password 입력하세요.");
         }
 
-        if (Ut.str.isBlank(nickname)) {
+        if (Ut.str.isBlank(requestBody.nickname)) {
             // return RsData.of("400-1", "nickname 입력하세요.");
             throw new GlobalException("400-3", "nickname 입력하세요.");
         }
 
-        return memberService.join(username, password, nickname);
+        return memberService.join(requestBody.username, requestBody.password, requestBody.nickname);
 
     }
 
 
     @GetMapping("/testThrowIllegalArgumentException")
-    @ResponseBody
     public RsData<Member> testThrowIllegalArgumentException() {
         throw new IllegalArgumentException("IllegalArgumentException");
     }
