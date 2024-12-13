d1a0e23739fc1855ee936cc35141c9965f468ccb
jby9725 <jby9725@gmail.com>
2024-12-13 10:07:23 +0900

    백엔드 통신용 타입스크립트 자동생성

--- src/main/java/com/koreait/surl_project_11/standard/util/Ut.java
+++ src/main/java/com/koreait/surl_project_11/standard/util/Ut.java
@@ -3,16 +3,16 @@ package com.koreait.surl_project_11.standard.util;
 import com.koreait.surl_project_11.global.app.AppConfig;
 import lombok.SneakyThrows;
 
+import java.util.concurrent.TimeUnit;
+
 public class Ut {
-    // 접근할 때 Ut.str.isBlank()
     public static class str {
         public static boolean isBlank(String str) {
             return str == null || str.isBlank();
         }
 
-        // 반대 기능 (습관적으로 작성)
-        // (비슷한 이름으로 같은 기능) public static boolean hasLength(String str) {}
-        public static boolean isNotBlank(String str) {
+        //public static boolean isNotBlank(String str) {}
+        public static boolean hasLength(String str) {
             return !isBlank(str);
         }
     }
@@ -21,6 +21,23 @@ public class Ut {
         @SneakyThrows
         public static String toString(Object obj) {
             return AppConfig.getObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+        }
+    }
+
+    public static class cmd {
+        public static void runAsync(String cmd) {
+            new Thread(() -> {
+                run(cmd);
+            }).start();
+        }
+        public static void run(String cmd) {
+            try {
+                ProcessBuilder processBuilder = new ProcessBuilder(cmd.split(" "));
+                Process process = processBuilder.start();
+                process.waitFor(1, TimeUnit.MINUTES);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
